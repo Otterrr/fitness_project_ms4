9673ea35c91d81b2242b616ac2f47d69ff08346d
Dan Otter <danco77@virginmedia.com>
2022-02-17 14:29:21 +0000

    Added products and category jsons, fixed admin to show correct info

---
+++ products/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)

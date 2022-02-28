0836437e50f09464a995cdbdcb52a08647da2fe6
Dan Otter <danco77@virginmedia.com>
2022-02-28 15:20:18 +0000

    Completed webhook handler, fixed bug on incorrect WH key

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,7 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
+    
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */

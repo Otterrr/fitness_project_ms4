8161e6af339c099c5546eab67c6afad4c2496c58
Dan Otter <danco77@virginmedia.com>
2022-03-01 14:04:11 +0000

    Simplified mobile-nav dropdown black overlay

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name="products"),
+    path('?category=equipment,tops,bottoms,accessories', views.all_products, name="products"),
     path('<product_id>', views.product_detail, name="product_detail"),
 ]

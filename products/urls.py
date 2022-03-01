b6a82025ed40f8b2880801ec13eaf83b56d2e33a
Dan Otter <danco77@virginmedia.com>
2022-03-01 20:55:47 +0000

    Added add_product template, view & url

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('?category=equipment,tops,bottoms,accessories', views.all_products, name="products"),
-    path('<product_id>', views.product_detail, name="product_detail"),
+    path('?category=equipment,tops,bottoms,accessories/', views.all_products, name="products"),
+    path('<int:product_id>/', views.product_detail, name="product_detail"),
+    path('add/', views.add_product, name="add_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -65,3 +67,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)

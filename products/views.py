2710517d0e88fe2832229083ac68a3bf9ada151e
Dan Otter <danco77@virginmedia.com>
2022-03-01 22:02:15 +0000

    added admin delete function

--- products/views.py
+++ products/views.py
@@ -74,9 +74,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added Product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -111,3 +111,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Product deleted!')
+    return redirect(reverse('products'))

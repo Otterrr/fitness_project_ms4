a37432515e7ec7a76fbea67891a2c2b4223d6fe2
Dan Otter <danco77@virginmedia.com>
2022-03-03 04:40:54 +0000

    final css, html changes

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse,
+    HttpResponse, get_object_or_404)
 from django.contrib import messages
 
 from products.models import Product
 
-# Create your views here.
+# Create your views here .
 
 
 def view_bag(request):
